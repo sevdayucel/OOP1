7c90f362e7f3ef0586ab15a9ed496d04c072c6c1
svdycl2858@gmail.com <svdycl2858@gmail.com>
2021-01-24 13:27:32 +0300

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        //prop çift tab ile public şablonu oluşur 
+        public int Id { get; set; } //primary key
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+    }
+}
---
+++ ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //ekle,sil,listele gibi operasyonlar burada gerçekleşiyor
+        public void Add(Product product) //product için verilen değer Product class ına atansın
+        {
+            //program.cs den verileri add ile almak için kullanılan alan
+            Console.WriteLine(product.ProductName + "eklendi");
+
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi");
+
+        }
+        
+    }
+}
